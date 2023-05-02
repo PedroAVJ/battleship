ea2eb085b0071efaec9e14a255dbe853a3cc7e91
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2023-05-02 12:43:47 -0600

    Chenged Vue config as Github Pages is not working

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  // Allows debugging in browser dev tools
   configureWebpack: {
     devtool: 'source-map'
-  }
+  },
+
+  // Needed for github pages to work
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/ybatlleship/'
+    : '/'
+
 })
