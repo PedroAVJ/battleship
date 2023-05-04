4df5a9e8493264a252830f86339edd2b810d4fb0
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2023-05-04 17:06:50 -0600

    Missing commits from the last push

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
-import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import { createApp } from 'vue'
 import { createPinia } from 'pinia';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-
-const pinia = createPinia()
-pinia.use(piniaPluginPersistedstate)
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // We use bundle because it includes popper.js, and that is required for tooltips to work
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+
+// Create pinia instance that uses the persistedstate plugin
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 
 createApp(App).use(pinia).use(router).mount('#app')
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,113 +4,109 @@ import Ship from '@/types/Ship';
 import Tile from '@/types/Tile';
 
 
-const state = new RootState();
-
-const actions = {
-    
-    // GUI
-    setGuiSubmarineCount(submarineCount: number) {
-        state.gui.submarineCount = submarineCount;
-    },
-    setGuiSupplyBoatCount(supplyBoatCount: number) {
-        state.gui.supplyBoatCount = supplyBoatCount;
-    },
-    setGuiDestroyerCount(destroyerCount: number) {
-        state.gui.destroyerCount = destroyerCount;
-    },
-    setGuiBattleshipCount(battleshipCount: number) {
-        state.gui.battleshipCount = battleshipCount;
-    },
-    setGuiFrigateCount(frigateCount: number) {
-        state.gui.frigateCount = frigateCount;
-    },
-    setGuiAircraftCarrierCount(aircraftCarrierCount: number) {
-        state.gui.aircraftCarrierCount = aircraftCarrierCount;
-    },
-
-    // Player
-    setPlayerSubmarineIsUsingAbility(isUsingAbility: boolean) {
-        state.player.submarine.isUsingAbility = isUsingAbility;
-    },
-    setPlayerSubmarineHasUsedAbility(hasUsedAbility: boolean) {
-        state.player.submarine.hasUsedAbility = hasUsedAbility;
-    },
-    setPlayerBattleshipIsUsingAbility(isUsingAbility: boolean) {
-        state.player.battleship.isUsingAbility = isUsingAbility;
-    },
-    setPlayerBattleshipHasUsedAbility(hasUsedAbility: boolean) {
-        state.player.battleship.hasUsedAbility = hasUsedAbility;
-    },
-    setPlayerBattleshipHealth(health: number) {
-        state.player.battleship.health = health;
-    },
-    setPlayerAircraftCarrierIsUsingAbility(isUsingAbility: boolean) {
-        state.player.aircraftCarrier.isUsingAbility = isUsingAbility;
-    },
-    setPlayerAircraftCarrierHasUsedAbility(hasUsedAbility: boolean) {
-        state.player.aircraftCarrier.hasUsedAbility = hasUsedAbility;
-    },
-    setPlayerAircraftCarrierHealth(health: number) {
-        state.player.aircraftCarrier.health = health;
-    },
-    setPlayerAircraftCarrierShots(shots: number) {
-        state.player.aircraftCarrier.shots = shots;
-    },
-    setPlayerIsMoveInProgress(isMoveInProgress: boolean) {
-        state.player.isMoveInProgress = isMoveInProgress;
-    },
-    setPlayerHasCurrentTurn(hasCurrentTurn: boolean) {
-        state.player.hasCurrentTurn = hasCurrentTurn;
-    },
-    setPlayerBoard(board: Tile[][]) {
-        state.player.board = board;
-    },
-
-    // Computer
-    setComputerSubmarineIsUsingAbility(isUsingAbility: boolean) {
-        state.computer.submarine.isUsingAbility = isUsingAbility;
-    },
-    setComputerSubmarineHasUsedAbility(hasUsedAbility: boolean) {
-        state.computer.submarine.hasUsedAbility = hasUsedAbility;
-    },
-    setComputerBattleshipIsUsingAbility(isUsingAbility: boolean) {
-        state.computer.battleship.isUsingAbility = isUsingAbility;
-    },
-    setComputerBattleshipHasUsedAbility(hasUsedAbility: boolean) {
-        state.computer.battleship.hasUsedAbility = hasUsedAbility;
-    },
-    setComputerBattleshipHealth(health: number) {
-        state.computer.battleship.health = health;
-    },
-    setComputerAircraftCarrierIsUsingAbility(isUsingAbility: boolean) {
-        state.computer.aircraftCarrier.isUsingAbility = isUsingAbility;
-    },
-    setComputerAircraftCarrierHasUsedAbility(hasUsedAbility: boolean) {
-        state.computer.aircraftCarrier.hasUsedAbility = hasUsedAbility;
-    },
-    setComputerAircraftCarrierHealth(health: number) {
-        state.computer.aircraftCarrier.health = health;
-    },
-    setComputerIsMoveInProgress(isMoveInProgress: boolean) {
-        state.computer.isMoveInProgress = isMoveInProgress;
-    },
-    setComputerHasCurrentTurn(hasCurrentTurn: boolean) {
-        state.computer.hasCurrentTurn = hasCurrentTurn;
-    },
-    setComputerBoard(board: Tile[][]) {
-        state.computer.board = board;
-    },
-
-    // Ship Preview
-    setShipPreview(ship: Ship) {
-        state.shipPreview = ship;
-    },
-}
-
 export const useStore = defineStore({
-  // unique id of the store across your application
-  id: 'rootStore',
-  state: () => new RootState(),
-  actions,
-  persist: true,
+    id: 'rootStore',
+    state: () => new RootState(),
+    actions: {
+
+        // GUI
+        setGuiSubmarineCount(submarineCount: number) {
+            this.gui.submarineCount = submarineCount;
+        },
+        setGuiSupplyBoatCount(supplyBoatCount: number) {
+            this.gui.supplyBoatCount = supplyBoatCount;
+        },
+        setGuiDestroyerCount(destroyerCount: number) {
+            this.gui.destroyerCount = destroyerCount;
+        },
+        setGuiBattleshipCount(battleshipCount: number) {
+            this.gui.battleshipCount = battleshipCount;
+        },
+        setGuiFrigateCount(frigateCount: number) {
+            this.gui.frigateCount = frigateCount;
+        },
+        setGuiAircraftCarrierCount(aircraftCarrierCount: number) {
+            this.gui.aircraftCarrierCount = aircraftCarrierCount;
+        },
+
+        // Player
+        setPlayerSubmarineIsUsingAbility(isUsingAbility: boolean) {
+            this.player.submarine.isUsingAbility = isUsingAbility;
+        },
+        setPlayerSubmarineHasUsedAbility(hasUsedAbility: boolean) {
+            this.player.submarine.hasUsedAbility = hasUsedAbility;
+        },
+        setPlayerBattleshipIsUsingAbility(isUsingAbility: boolean) {
+            this.player.battleship.isUsingAbility = isUsingAbility;
+        },
+        setPlayerBattleshipHasUsedAbility(hasUsedAbility: boolean) {
+            this.player.battleship.hasUsedAbility = hasUsedAbility;
+        },
+        setPlayerBattleshipHealth(health: number) {
+            this.player.battleship.health = health;
+        },
+        setPlayerAircraftCarrierIsUsingAbility(isUsingAbility: boolean) {
+            this.player.aircraftCarrier.isUsingAbility = isUsingAbility;
+        },
+        setPlayerAircraftCarrierHasUsedAbility(hasUsedAbility: boolean) {
+            this.player.aircraftCarrier.hasUsedAbility = hasUsedAbility;
+        },
+        setPlayerAircraftCarrierHealth(health: number) {
+            this.player.aircraftCarrier.health = health;
+        },
+        setPlayerAircraftCarrierShots(shots: number) {
+            this.player.aircraftCarrier.shots = shots;
+        },
+        setPlayerIsMoveInProgress(isMoveInProgress: boolean) {
+            this.player.isMoveInProgress = isMoveInProgress;
+        },
+        setPlayerHasCurrentTurn(hasCurrentTurn: boolean) {
+            this.player.hasCurrentTurn = hasCurrentTurn;
+        },
+        setPlayerBoard(board: Tile[][]) {
+            this.player.board = board;
+        },
+
+        // Computer
+        setComputerSubmarineIsUsingAbility(isUsingAbility: boolean) {
+            this.computer.submarine.isUsingAbility = isUsingAbility;
+        },
+        setComputerSubmarineHasUsedAbility(hasUsedAbility: boolean) {
+            this.computer.submarine.hasUsedAbility = hasUsedAbility;
+        },
+        setComputerBattleshipIsUsingAbility(isUsingAbility: boolean) {
+            this.computer.battleship.isUsingAbility = isUsingAbility;
+        },
+        setComputerBattleshipHasUsedAbility(hasUsedAbility: boolean) {
+            this.computer.battleship.hasUsedAbility = hasUsedAbility;
+        },
+        setComputerBattleshipHealth(health: number) {
+            this.computer.battleship.health = health;
+        },
+        setComputerAircraftCarrierIsUsingAbility(isUsingAbility: boolean) {
+            this.computer.aircraftCarrier.isUsingAbility = isUsingAbility;
+        },
+        setComputerAircraftCarrierHasUsedAbility(hasUsedAbility: boolean) {
+            this.computer.aircraftCarrier.hasUsedAbility = hasUsedAbility;
+        },
+        setComputerAircraftCarrierHealth(health: number) {
+            this.computer.aircraftCarrier.health = health;
+        },
+        setComputerIsMoveInProgress(isMoveInProgress: boolean) {
+            this.computer.isMoveInProgress = isMoveInProgress;
+        },
+        setComputerHasCurrentTurn(hasCurrentTurn: boolean) {
+            this.computer.hasCurrentTurn = hasCurrentTurn;
+        },
+        setComputerBoard(board: Tile[][]) {
+            this.computer.board = board;
+        },
+
+        // Ship Preview
+        setShipPreview(ship: Ship) {
+            this.shipPreview = ship;
+        },
+
+    },
+    persist: true,
 });
